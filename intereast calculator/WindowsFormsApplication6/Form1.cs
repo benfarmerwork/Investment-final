56390e874287f28f7eed5749ec546f7044590c37
Ben Farmer <ben_farmer3@hotmail.com>
2016-07-30 00:01:00 +0100

    Final

---
+++ intereast calculator/WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int years;
+        int amount_per_year;
+        double final = 0;
+        double start = 0;
+        double interest = 0;
+        double total = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            years = int.Parse(textBox1.Text);
+            amount_per_year = int.Parse(textBox2.Text);
+            listBox1.Items.Add("year  Start   Paid in   Intereast  Final");
+            for (int i = 1; i < years+1; i++)
+            {
+                interest = (final + amount_per_year) * 0.10;
+                final = (final + amount_per_year) * 1.10;
+                
+                listBox1.Items.Add(i.ToString() + "       " + start +  "         " + amount_per_year + "         " +  interest + "         " + final);
+                start = final ;
+            }
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
